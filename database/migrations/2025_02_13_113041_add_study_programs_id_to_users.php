52b9d9d403619a9b1e41ddcf17f84cb82bda0286
Rahadian Fahri Faizani <69681505+ashtrath@users.noreply.github.com>
2025-02-14 11:57:31 +0700

    fix: missing signature image (study_program)

---
+++ database/migrations/2025_02_13_113041_add_study_programs_id_to_users.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('study_program_id')->nullable()->after('password')->constrained('study_programs')->nullOnDelete();
+
+            $table->index('study_program_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
